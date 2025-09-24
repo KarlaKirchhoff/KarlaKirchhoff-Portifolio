af4f08f1da2901ac5274b4326d15b56393e5036f
Chellp <karlakirchhoff.contato@gmail.com>
2025-09-24 18:59:46 -0300

    refactor: migrando para react

---
+++ web/src/pages/CurriculoPages/Index/ModalCurso.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './modal.module.css'
+
+export default function ModalCurso({ onFechar, dados }) {
+
+    const handleFechar = () => {
+        onFechar();
+    };
+
+    return (
+        <section class="modal" id="index-modal">
+            <div class="area-botao">
+                <h2 id="h2-modal">{dados.nome}</h2>
+                <button id="modal-close-button"
+                onClick={handleFechar}>x</button>
+            </div>
+            <div class="img-area">
+                <img id="img-modal" href={dados.img} />
+            </div>
+        </section>
+    )
+}
--- web/src/pages/CurriculoPages/Index/index.jsx
+++ web/src/pages/CurriculoPages/Index/index.jsx
@@ -1,22 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './index.module.css'
 import Header from '../../../shared/componentes/Header';
+import Modal from './ModalCurso';
 
 import Perfil from '../../../models/Perfil';
 const perfil = new Perfil();
 
 export default function CurriculoPage() {
 
+    const [openInfoCurso, setOpenInfoCurso] = useState(false);
+    const [dadosCurso, setDadosCurso] = useState(undefined);
+    const [openCertificado, setOpenCertificado] = useState(false);
+    const [dadosCertificado, setDadosCertificado] = useState(undefined);
+
+    function toggleAtivo() {
+        const modal = document.getElementById('index-modal');
+        modal.classList.toggle('ativo');
+    }
+
+    const handleVerInfoCurso = () => {
+        setOpenInfoCurso(true)
+        toggleAtivo()
+    }
+
+    const handleFecharInfoCurso = () => {
+        toggleAtivo()
+        setOpenInfoCurso(false)
+    }
+
+    const handleVerCertificado = (dados) => {
+        setDadosCertificado(dados);
+        setOpenCertificado(true)
+    }
+
+    const handleFecharCertificado = () => {
+        setOpenCertificado(false)
+    }
+
     const interesses = perfil.interesses;
     const cursos = perfil.cursos;
 
     return (
         <>
-            <main class="container">
+            <main className={styles.container}>
                 <Header />
-                <section class="sobre-mim" id="sobreMim-index">
+                <section className={styles["sobre-mim"]} id="sobreMim-index">
 
-                    <div className="habilidades grid-icons" id="sobreMim-interesses" >
+                    <div className={styles["habilidades grid-icons"]} id="sobreMim-interesses" >
                         {interesses.map((i, idx) => (
                             <div key={idx}>
                                 <ion-icon name={i.icone} />
@@ -25,71 +55,71 @@ export default function CurriculoPage() {
                         ))}
                     </div>
 
-                    <div className="texto" id="sobreMim-texto">
+                    <div className={styles.texto} id="sobreMim-texto">
                         <h2>Sobre Mim</h2>
                         <p>{perfil.sobreMim_p1}</p>
                         <p>{perfil.objetivoEmpresa}</p>
                     </div>
 
-                    <section className="sec-escolaridade sec-list">
+                    <section className={styles["sec-escolaridade sec-list"]}>
                         <ul>
                             {cursos.map((i, idx) => (
-                                <li key={idx} className="list-item">
-                                    <div className="info-list-item" style={{ backgroundColor: "var(--cor-branco-transp)" }}>
-                                        <p className="nome-instituicao">{i.instituicao}</p>
-                                        <p className="nome-curso">{i.nome}</p>
-                                        <p className="duracao">{i.duracao}</p>
+                                <li key={idx} className={styles["list-item"]}>
+                                    <div className={styles["info-list-item"]} style={{ backgroundColor: "var(--cor-branco-transp)" }}>
+
+                                        <p className={styles["nome-instituicao"]}>{i.instituicao}</p>
+                                        <p className={styles["nome-curso"]}>{i.nome}</p>
+                                        <p className={styles["duracao"]}>{i.duracao}</p>
+
                                     </div>
 
-                                    <span className="hover">...</span>
+                                    <span className="hover"
+                                        onClick={() => handleVerInfoCurso}>...</span>
 
-                                    <div className="info-curso" id='id'>
-                                        <div className="desc-div">
-                                            <h4>Desrição:</h4>
-                                            <p className="descricao"></p>
-                                        </div>
+                                    {openInfoCurso && (
+                                        <div className={styles["info-curso"]} id='id'>
+                                            <div className={styles["desc-div"]}>
+                                                <h4>Desrição:</h4>
+                                                <p className={styles["descricao"]}>{i.desc}</p>
+                                            </div>
 
-                                        <div className="grade-div">
-                                            <h4>Grade:</h4>
-                                            <ol>
-                                                {i.grade.map((grade, index) => (
-                                                    <li key={`grade_${index}`}>{grade}</li>
-                                                ))}
-                                            </ol>
+                                            <div className={styles["grade-div"]}>
+                                                <h4>Grade:</h4>
+                                                <ol>
+                                                    {i.grade.map((grade, index) => (
+                                                        <li key={`grade_${index}`}>{grade}</li>
+                                                    ))}
+                                                </ol>
+                                            </div>
+                                            <img src={i.img} alt="" onClick={() => handleVerCertificado({ nome: i.nome, img: i.img })} />
+                                            <button className={styles['fechar-item hover']}
+                                                onClick={() => handleFecharInfoCurso}>X</button>
                                         </div>
-                                        <img src={i.img} alt="" />
-                                        <button className='fechar-item hover'>X</button>
-                                    </div>
+                                    )}
                                 </li>
                             ))}
                         </ul>
                     </section>
 
-                    <div className="tecnologias grid-icons" id="sobreMim-tecnologias">
+                    <div className={styles["tecnologias grid-icons"]} id="sobreMim-tecnologias">
                     </div>
 
-                    <section class="projetos">
+                    <section className={styles["projetos"]}>
                         <h2>Projetos</h2>
 
-                        <div class="cards" id="cards-projetos"></div>
+                        <div className={styles["cards"]} id="cards-projetos"></div>
 
                     </section>
-
                 </section>
 
             </main >
+            <footer className="rodape"></footer>
 
-            <footer class="rodape"></footer>
-
-            <section class="modal" id="index-modal">
-                <div class="area-botao">
-                    <h2 id="h2-modal">Nome Projeto</h2>
-                    <button id="modal-close-button">x</button>
-                </div>
-                <div class="img-area">
-                    <img id="img-modal" />
-                </div>
-            </section>
+            {openCertificado && (
+                <Modal
+                    onFechar={handleFecharCertificado}
+                    dados={dadosCertificado} />
+            )}
         </>
     )
 
