61ef1264feed4eabfa579fe99b65c7b3744a9ea0
Chellp <karlakirchhoff.contato@gmail.com>
2025-10-03 19:10:13 -0300

    epdateupdate: criação e responsividade do component 'hero' da landing page

--- web/src/pages/LandingPage/HeroSection/index.jsx
+++ web/src/pages/LandingPage/HeroSection/index.jsx
@@ -4,9 +4,32 @@ import styles from './index.module.css'
 
 export default function Hero_LandingPage() {
     return (
-        <div className={styles.container}>
-            <h1>Construa seu site no futuro</h1>
-            <h2>Sites com design inovador, experiência única e visual de tirar o fôlego</h2>
-        </div>
+        <section className={styles.container}>
+            <div className={styles["titulo-area"]}>
+                <h1>Desenvolvimento Web Fullstack, Design e Sistemas Mobile</h1>
+            </div>
+            <div className={styles["txt-area"]}>
+                <h2>Construa seu site no futuro</h2>
+                <div className={styles["grid-area"]}>
+                    <a href="#" className={`${styles.area} ${styles.glass}`}>
+                        <img src="./img/gif/homem-programando.gif" alt="" />
+                        <p>Portifolio</p>
+
+                    </a>
+                    <a href="#" className={`${styles.area} ${styles.glass}`}>
+                        <img src="./img/gif/human-robot-inteligencia-artificial.gif" alt="" />
+                        <p>Serviços</p>
+                    </a>
+                    <a href="#" className={`${styles.area} ${styles.glass} ${styles.area1}`}>
+                        <img src="./img/gif/inteligencia-artificial-gif.gif" alt="" />
+                        <p>Contato</p>
+                    </a>
+                </div>
+
+                <a href="#" className={styles.cta}>
+                    <button>Transforme Sua Ideia em Realidade Digital</button>
+                </a>
+            </div>
+        </section>
     )
 }
